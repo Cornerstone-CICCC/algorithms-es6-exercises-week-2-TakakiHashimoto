a67d9b88239d161536a5d0a521054fd17ee2fab4
gooko <takakihashimotogoo@gmail.com>
2025-09-24 18:26:27 -0700

    ex04

--- ex04.js
+++ ex04.js
@@ -2,15 +2,46 @@
 In this exercise, we will be given a list of instructors and have to determine which instructor has the longest name.
 
 Instruction
-Create a function named instructorWithLongestName that will receive an array of instructor objects, and return the object that has the longest name. If there are two instructors with the longest name, return the first one.
+Create a function named instructorWithLongestName that will receive an array of instructor objects, 
+and return the object that has the longest name. If there are two instructors with the longest name, return the first one.
 */
 
+
+// Normal for loop
 const instructorWithLongestName = function (instructors) {
   // Put your solution here
+  let wordCounter = 0;
+  let highestChars = 0;
+  let heighestObj;
+  for (const obj of instructors){
+    wordCounter = 0;
+    for (const char of obj.name){
+      wordCounter += 1;
+    }
+    if (wordCounter > highestChars){
+      highestChars = wordCounter;
+      heighestObj = obj;
+    }
+  }
+
+  return heighestObj
 };
 
+
+// with .reduce()
+const instructorWithLongestName2 = function (instructors) {
+  // Put your solution here
+  const longestName = instructors.reduce((acc, curr) =>{
+    return curr.name.length > acc.name.length ? curr : acc
+  }, instructors[0])
+
+  return longestName
+};
+
+
+
 console.log(
-  instructorWithLongestName([
+  instructorWithLongestName2([
     { name: "Samuel", course: "Mobile" },
     { name: "Jeremiah", course: "Web" },
     { name: "Ophilia", course: "Web" },
@@ -19,7 +50,7 @@ console.log(
 ); //{name: "Jeremiah", course: "Web"}
 
 console.log(
-  instructorWithLongestName([
+  instructorWithLongestName2([
     { name: "Matthew", course: "Web" },
     { name: "David", course: "Mobile" },
     { name: "Domascus", course: "Web" },
