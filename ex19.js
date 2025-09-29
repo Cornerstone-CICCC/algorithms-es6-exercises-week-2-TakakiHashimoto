a9ccd4735b3f921cc95e0e1a5dedbdb1389ca950
gooko <takakihashimotogoo@gmail.com>
2025-09-28 19:51:50 -0700

    completed ex19

--- ex19.js
+++ ex19.js
@@ -1,11 +1,14 @@
 /*In this exercise we will be writing an algorithm, to detect if two queens on a chess board can attack each other.
 
 Queen Threat Detector
-A game of chess is played on an 8 column by 8 row board. In the game of chess, a queen can attack pieces which are on the same row, column, or diagonal.
+A game of chess is played on an 8 column by 8 row board. In the game of chess, 
+a queen can attack pieces which are on the same row, column, or diagonal.
 
 Chess Board Queen
 
-In JavaScript, we can represent a chessboard using an 8 by 8 array (8 arrays within an array). For this exercise, our chess board will have 2 queens, and nothing else. A 1 in the array represents a queen on the corresponding square, and a O in the array represents an unoccupied square.
+In JavaScript, we can represent a chessboard using an 8 by 8 array (8 arrays within an array). 
+For this exercise, our chess board will have 2 queens, and nothing else. 
+A 1 in the array represents a queen on the corresponding square, and a O in the array represents an unoccupied square.
 
 So the following chess board:
 
@@ -23,7 +26,8 @@ Would be represented in JavaScript like this:
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0]
 ]
-Our first challenge will be to write a function that generates a chess board like this. We will then write a function to detect weather or not the two queens are positioned so that they attack each other.
+Our first challenge will be to write a function that generates a chess board like this. 
+We will then write a function to detect weather or not the two queens are positioned so that they attack each other.
 
 let whiteQueen = [0, 5];
 let blackQueen = [5, 0];
@@ -66,3 +70,61 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+function generateBoard(arr1, arr2){
+  let chessBoard = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+  ]
+  for (let row in chessBoard){
+    for (let col in chessBoard[row]){
+      if (row == arr1[0] && col == arr1[1]){
+          chessBoard[row][col] = 1;
+      }
+    }
+  }
+
+    for (let row in chessBoard){
+    for (let col in chessBoard[row]){
+      if (row == arr2[0] && col == arr2[1]){
+          chessBoard[row][col] = 1;
+      }
+    }
+  }
+
+  return chessBoard;
+}
+
+function queenThreat(board){
+  let location1 = [];
+  for (let rowStr in board){
+    const row = parseInt(rowStr)
+    for (let colStr in board[row]){
+      const col = parseInt(colStr);
+      if (board[row][col] == 1){
+        location1.push(row, col)
+      }
+    }
+  }
+
+
+  if ((location1[0] == location1[2]) || (location1[1] == location1[3]) || 
+      (Math.abs(location1[0] - location1[2]) == Math.abs(location1[1] - location1[3]))){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
